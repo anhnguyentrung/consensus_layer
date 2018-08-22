4146f75ee535ce6311ba2d4079e72120eab448c4
anhnguyentrung <nguyentrunganh.bk@gmail.com>
2018-08-22 18:24:03 +0900

    packet and send message

--- network/connection.go
+++ network/connection.go
@@ -3,24 +3,52 @@ package network
 import (
 	"net"
 	"bufio"
-	"fmt"
 )
 
 type Connection struct {
-	conn net.Conn
-	connReader *bufio.Reader
-	isOpen bool
-	isSynchronized bool
+	conn 			net.Conn
+	connReader 		*bufio.Reader
+	isOpen 			bool
+	isSynchronizing bool
+	peerAddress		string
 }
 
-func (c *Connection) sendMessage(message Message) {
-	buf, err := marshalBinary(message.Content)
+func NewConnection(peerAddress string) *Connection {
+	return &Connection{
+		peerAddress:		peerAddress,
+		isOpen:				false,
+		isSynchronizing:	false,
+	}
+}
+
+func (c *Connection) sendPacket(packet interface{}) error {
+	err := error(nil)
+	bytes := make([]byte, 0)
+	messageType := Handshake
+	switch packet.(type) {
+	case HandshakePacket:
+		bytes, err = marshalBinary(packet)
+		if err != nil {
+			return err
+		}
+	}
+	message := Message{
+		Header: MessageHeader{},
+		Payload: bytes,
+	}
+	message.Header.Type = messageType
+	message.Header.Length = uint32(len(bytes))
+	data, err := marshalBinary(message)
 	if err != nil {
-		fmt.Println("Encode content: ", err)
 		return err
 	}
-	message.Header.Length = uint32(len(buf))
-	data, err := marshalBinary(message)
-	c.Conn.Write(data)
+	c.conn.Write(data)
 	return err
+}
+
+func (c *Connection) close()  {
+	c.connReader = nil
+	c.isOpen = false
+	c.isSynchronizing = false
+	c.conn.Close()
 }
--- network/types.go
+++ network/types.go
@@ -1,5 +1,7 @@
 package network
 
+import "consensus_layer/crypto"
+
 type MessageType byte
 const (
 	Handshake MessageType = iota
@@ -8,6 +10,14 @@ const (
 	Block
 )
 
+type NetworkType byte
+const (
+	TestNet NetworkType = iota
+	MainNet
+)
+
+type SHA256Type [32]byte
+
 type MessageHeader struct {
 	Type 	MessageType // 1 byte
 	Length  uint32  	// 4 bytes
@@ -15,4 +25,22 @@ type MessageHeader struct {
 type Message struct {
 	Header MessageHeader
 	Payload []byte
+}
+
+type HandshakeInfo struct {
+	Network					NetworkType
+	Version					uint16
+	ChainId                 SHA256Type
+	NodeId                  SHA256Type
+	Key                     crypto.PublicKey
+	P2PAddress              string
+	LastCommitBlockHeight   uint32
+	LastCommitBlockId  		SHA256Type
+	TopBlockHeight          uint32
+	TopBlockId              SHA256Type
+}
+
+type HandshakePacket struct {
+	Info HandshakeInfo
+	Sign crypto.Signature
 }
